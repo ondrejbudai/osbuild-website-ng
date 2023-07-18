91804f0f8b0509cc45820bfdf7fe168c8d930e60
Ondřej Budai <ondrej@budai.cz>
2023-07-18 17:49:40 +0200

    Set configs for correct deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,14 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/osbuild-website-ng/',
+
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'osbuild-website-ng',
+  organizationName: 'ondrejbudai',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
